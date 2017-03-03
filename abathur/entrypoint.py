e5bb7ad17a8448fb4baf2227402515872596ceff
yeyuexia <yyxworld@gmail.com>
2017-03-04 00:02:58 +0800

    fix entrypoint

--- abathur/entrypoint.py
+++ abathur/entrypoint.py
@@ -1,29 +1,8 @@
 # coding: utf8
 
-import argparse
-
-from abathur import Builder
-
-DESCRIPTION = """
-Abathur help you build project based on template.
-"""
-DEFAULT_TEMPLATE_PATH = "template"
-
-
-def generate_argument_parser():
-    parser = argparse.ArgumentParser(
-        prog="abathur", description=DESCRIPTION
-    )
-    parser.add_argument("command", required=True)
-    parser.add_argument("args", nargs="*")
+from abathur.command import get_parser
 
 
 if __name__ == "__main__":
-    args = generate_argument_parser().parse_args()
-
-    template_path = args.template or DEFAULT_TEMPLATE_PATH
-    dest_root = args.output or args.project_name
-    Builder(
-        args.project_name, template_path, dest_root,
-        args.placeholder, args.file
-    ).build()
+    args = get_parser().parse_args()
+    args.handle(args)
