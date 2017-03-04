f2acd6f3188dcdc8d362d715a0c7b6ba70873f2f
yeyuexia <yyxworld@gmail.com>
2017-03-04 17:37:22 +0800

    remove nargs in commands

--- abathur/commands/alias_command.py
+++ abathur/commands/alias_command.py
@@ -10,8 +10,8 @@ class AliasCommand(Command):
         super().__init__("alias", "alias help")
 
     def add_custom_options(self, parser):
-        parser.add_argument("alias", nargs=1, help="")
-        parser.add_argument("uri", nargs=1, help="")
+        parser.add_argument("alias",  help="")
+        parser.add_argument("uri", help="")
 
     def handle(self, args):
         manager = AliasManager()
