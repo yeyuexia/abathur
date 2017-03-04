393b3b3b9288f2605dad4b0567e490276ea4dda8
yeyuexia <yyxworld@gmail.com>
2017-03-04 17:40:05 +0800

    extract root dir in constant

--- abathur/constant.py
+++ abathur/constant.py
@@ -1,6 +1,9 @@
 # coding: utf8
 
-ALIAS_CONFIGURATION = "~/.abathur/alias.json"
+from os import path
+
+ABATHUR_CONFIGURATION_ROOT = path.join(path.expanduser("~"), ".abathur")
+ALIAS_CONFIGURATION = path.join(ABATHUR_CONFIGURATION_ROOT, "alias.json")
 ABATHUR_CONFIGURATION = ".abathur"
 IGNORE_FILES = [r"\.git/.*", r".abathur"]
 REMOTE_RESOURCE_HEADER = ["git@", "https://"]
