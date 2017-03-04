5ef7a44bb476e3c94d4ee63615030891a27ebbd2
yeyuexia <yyxworld@gmail.com>
2017-03-04 17:30:45 +0800

    check input args

--- abathur/entrypoint.py
+++ abathur/entrypoint.py
@@ -1,9 +1,14 @@
 # coding: utf8
 
+import sys
+
 from abathur.commands import get_parser
 
 
 if __name__ == "__main__":
+    parser = get_parser()
+    if len(sys.argv[1:]) == 0:
+        parser.print_help()
+        parser.exit()
     args = get_parser().parse_args()
-    print(dir(args))
     args.handle(args)
