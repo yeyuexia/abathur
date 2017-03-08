929c5ff2c7b50cf9bae251ff38617999f49626ee
yeyuexia <yyxworld@gmail.com>
2017-03-08 18:12:28 +0800

    fix ignore

--- abathur/constant.py
+++ abathur/constant.py
@@ -5,7 +5,7 @@ from os import path
 ABATHUR_CONFIGURATION_ROOT = path.join(path.expanduser("~"), ".abathur")
 ALIAS_CONFIGURATION = path.join(ABATHUR_CONFIGURATION_ROOT, "alias.json")
 ABATHUR_CONFIGURATION = ".abathur"
-IGNORE_FILES = [r"(.*/)?/\.git/.*", r".abathur"]
+IGNORE_FILES = [r"(.*/)?\.git.*", r"(.*/)?.abathur"]
 REMOTE_RESOURCE_HEADER = [r"^(https://|ssh://|http://)?(git@)?.*\.git$"]
 LOCAL_RESOURCE = "__local_resource__"
 REMOTE_RESOURCE = "__remote_resource__"
