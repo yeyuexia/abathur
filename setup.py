2a427cb135607fb02281cb177d6384cbd9fd0714
yeyuexia <yyxworld@gmail.com>
2017-05-31 16:11:57 +0800

    upgrade version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name="abathur",
-    version="1.0.0",
+    version="1.0.1",
     description="Template manager",
     long_description=long_description,
     url="https://github.com/yeyuexia/abathur",
