951e2a3cf5ba6a05f915f5581ad312d85536be06
yeyuexia <yyxworld@gmail.com>
2018-11-02 21:20:32 +0800

    fix indentation error

--- tests/alias_test.py
+++ tests/alias_test.py
@@ -60,7 +60,7 @@ class AliasManagerTest(TestCase):
 
     @patch("abathur.alias.os")
     def test_success_alias_by_name(self, mock_os):
-		mock_os.path.abspath.return_value = "test"
+        mock_os.path.abspath.return_value = "test"
         self.assertEqual(self.manager.get("alias").to_dict(), dict(
             uri="test", uri_type="__local_resource__"
         ))
